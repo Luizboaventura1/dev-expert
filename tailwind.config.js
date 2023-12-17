a011b44f4efa5aa4c9e844614746ac6209e0a21f
Luiz Henrique <luizboaventura24@gmail.com>
2023-12-17 08:49:55 -0300

    Página de venda

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        primaryColor: "#15d47b",
+        primaryColorHover: "#17ad67",
+        secondaryColor: "#121214",
+        thirdColor: "#202024",
+        textPrimaryColor: "#EEEEEE",
+        textSecondaryColor: "",
+      }
+    }
+  },
+  darkMode: 'class',
+}
